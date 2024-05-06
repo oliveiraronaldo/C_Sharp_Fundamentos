dbe9f5e58c212a79ba6fca3dcd8c516fd37facd5
Ronaldo Oliveira <ronaldofilh.98@gmail.com>
2024-05-06 20:30:27 -0300

    Add Moedas

---
+++ Moedas/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Globalization;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Clear();
+
+        decimal valor = 10.234M;
+
+        Console.WriteLine(valor);
+        var us = CultureInfo.CreateSpecificCulture("en-US");
+        var br = CultureInfo.CreateSpecificCulture("pt-BR");
+        var es = CultureInfo.CreateSpecificCulture("es-ES");
+        var cn = CultureInfo.CreateSpecificCulture("zh-CN");
+
+        Console.WriteLine(valor.ToString("C", br));     //C = currencie
+
+        Console.WriteLine(Math.Round(valor));
+        Console.WriteLine(Math.Ceiling(valor));
+
+    }
+}
